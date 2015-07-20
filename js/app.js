d9d3fbb5dccef2cd3d34c39e92b80d6e60a8ace7
Pablo Ledesma <pabloledes83@gmail.com>
2015-07-20 18:12:23 -0500

    finalización

--- js/app.js
+++ js/app.js
@@ -1,81 +1,17 @@
 new Vue({
-	el: '#tasks',
+	el: '#demo',
 	data: {
-		tasks: [],
-
-		newTask: ''
+		name: 'foobar'
 	},
 
-	computed: {
-		completions: function(){
-			return this.tasks.filter(function(task){
-				return task.completed;
-			});
-		},
+	ready: function(){
 
-		remaining: function(){
-			return this.tasks.filter(function(task){
-				return ! task.completed;
-			});
-		}
-	},
+		var that = this;
 
-
-
-	filters: {
-		// Return the ones is a completed is set to false
-		inProcess: function(tasks){
-			return tasks.filter(function(task){
-				return !task.completed;
-			});
-		}
-	},
-
-	methods: {
-		addTask: function(e){
-			e.preventDefault();
-
-			if( ! this.newTask ) return;
-
-			this.tasks.push({
-				body: this.newTask,
-				completed: false
-			});
-
-			this.newTask = '';
-		},
-
-		editTask: function(task){
-			//remove task
-			this.removeTask(task);
-
-			//update the newTask input
-			this.newTask = task.body;
-
-			//Focus the input field
-			this.$$.newTask.focus();
-		},
-
-		toggleTaskCompletion: function(task){
-			task.completed = ! task.completed;
-		},
-
-		completeAll: function(){
-			this.tasks.forEach(function(task){
-				task.completed = true;
-			});
-		},
-
-		removeTask: function(task){
-			this.tasks.$remove(task);
-
-		},
-
-		clearCompleted: function(){
-			this.tasks = this.tasks.filter(function(task){
-				return ! task.completed;
-			});
-		}
-
+		setInterval(function(){
+			that.name = 'Updated';
+		}, 5000);
 	}
+
+	
 })
