c2db1318e1473a7a1010fc01a360b83967d41d18
Pablo Ledesma <pabloledes83@gmail.com>
2015-07-20 22:09:47 -0500

    finish

--- js/app.js
+++ js/app.js
@@ -2,10 +2,29 @@ new Vue({
 	el: '#demo',
 	data: {
 		
+		columns: ['name', 'age'],
+
+		people: [
+			{name: 'John Doe', age: 20},
+			{name: 'Pablo Ledesma', age: 32},
+			{name: 'Agelica Lozano', age: 19},
+			{name: 'Federico Aristisabal', age: 35},
+			{name: 'Alam Brito', age: 40},
+			{name: 'Tito Puentes', age: 21},
+		],
+
+		sortKey: '',
+
+		reverse: false,
+
+		search: ''
 	}, 
 
 	methods: {
-		
+		sortBy: function(key){
+			this.reverse = (this.sortKey == key) ? ! this.reverse : false;
+			this.sortKey = key;
+		}
 	}
 	
 });
